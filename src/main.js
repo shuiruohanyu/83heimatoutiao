249e6b2ca6970223f7ebd4bcd5ba10ab2878bba2
gaoly <shuiruohanyu@foxmail.com>
2019-09-18 09:44:44 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 相当于全局注册elementUI  Vue.use => 干了什么?
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/home/index.vue' // 路由级组件
+import Home from './views/home/'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // 一级路由
     {
       path: '/',
-      name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue') // 按需加载
+    // }
   ]
 })
