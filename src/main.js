96297331b514e4c0306a22c16913149442b2dea3
gaoly <shuiruohanyu@foxmail.com>
2019-09-18 09:18:33 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 相当于全局注册elementUI  Vue.use => 干了什么?
 new Vue({
   router,
   render: h => h(App)
