d77b0115585eba8737e2cac40db94f8ba400f0d0
gaoly <shuiruohanyu@foxmail.com>
2019-09-18 16:04:02 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class='login'>我是主页</div>
+<!-- 先定义一个大容器 -->
+  <el-container>
+    <!-- 先放置一个左侧 -->
+    <el-aside>左侧内容</el-aside>
+    <!-- 右侧大容器 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>头部</el-header>
+      <!-- 中部区域 -->
+      <el-main>
+        <!-- 二级路由容器 -->
+         <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
