35346f8eee2bd89f999ac98241ee388db6c13e59
gaoly <shuiruohanyu@foxmail.com>
2019-09-18 10:09:39 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/home/index.vue' // 路由级组件
+// import Home from './views/home/index.vue' // 完整写法
 import Home from './views/home/'
-
+import Login from './views/login' // 简写
 Vue.use(Router)
 
 export default new Router({
@@ -10,7 +10,14 @@ export default new Router({
     // 一级路由
     {
       path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
       component: Home
+    }, {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
