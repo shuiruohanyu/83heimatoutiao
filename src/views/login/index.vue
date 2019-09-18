741c478165bc89d7da83505fb854bb3415bb0a1b
gaoly <shuiruohanyu@foxmail.com>
2019-09-18 15:30:16 +0800

    黑马头条PC-登录模块-登录请求-返回值中token解析-及token缓存

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -76,9 +76,18 @@ export default {
     login () {
       // 校验整个表单的规则
       // validate 是一个方法 => 方法中传入的一个函数 两个校验参数  是否校验成功/未校验成功的字段
-      this.$refs.myForm.validate(function (isOK) {
+      this.$refs.myForm.validate((isOK) => {
         if (isOK) {
-          console.log('校验成功')
+          this.$axios({
+            method: 'post',
+            url: '/authorizations',
+            data: this.loginForm
+          }).then(result => {
+            // 将后台返回的token令牌存储到前端缓存中
+            window.localStorage.setItem('user-token', result.data.data.token)
+          }).catch(error => {
+            console.log(error.message)
+          })
         }
       })
     }
