fe90f3ad71bcc4cc51063deb4d820afc50f8b517
gaoly <shuiruohanyu@foxmail.com>
2019-09-21 10:49:53 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,11 +9,15 @@
         <!-- el-table 表格 data属性是一个对象数组-->
         <el-table :data="list">
             <!-- el-table-column 列 属性 prop => 键名 => 字段名 label => 表头 -->
-            <el-table-column width="600px" label="标题" prop="title"></el-table-column>
-            <el-table-column align="center" label="评论状态" prop="comment_status"></el-table-column>
+            <el-table-column width="600px"  label="标题" prop="title"></el-table-column>
+            <el-table-column :formatter="stateFormatter" align="center" label="评论状态" prop="comment_status"></el-table-column>
             <el-table-column align="center" label="总评论数" prop="total_comment_count" ></el-table-column>
             <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count" ></el-table-column>
-            <el-table-column align="center" label="操作"  ></el-table-column>
+            <el-table-column align="center" label="操作"  >
+                <!-- 自定义内容 获取其他列的数据 -->
+                <el-button size="small" type='text'>修改</el-button>
+                <el-button size="small" type='text'>关闭评论</el-button>
+            </el-table-column>
 
         </el-table>
    </el-card>
@@ -35,6 +39,10 @@ export default {
       }).then(result => {
         this.list = result.data.results // 把返回的数据赋值给list
       })
+    },
+    // filter => return
+    stateFormatter (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
     }
   },
   created () {
