3d2ca0229141b43a803ff540a5e6c4191980f82f
shuiruohanyu <shuiruohanyu@foxmail.com>
2019-09-23 12:06:24 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -32,6 +32,25 @@
           </el-date-picker>
             </el-form-item>
         </el-form>
+        <!-- 主体内容 -->
+     <div class='total'>共找到55091条符合条件的内容</div>
+     <div class='article-item' v-for="(item,index) in list" :key="index">
+         <!-- 布局 -->
+         <!-- 左侧 -->
+        <div class='left'>
+            <img src="../../assets/img/404.png" alt="">
+            <div class='info'>
+                <span class='title'>十一放假,大家不开心</span>
+                <el-tag class='status'>已发表</el-tag>
+                <span class='date'>2019-09-23 11:21:21</span>
+            </div>
+        </div>
+        <!-- 右侧 -->
+        <div class='right'>
+            <span><i class="el-icon-edit"></i>修改</span>
+            <span><i class="el-icon-delete"></i>删除</span>
+        </div>
+     </div>
     </el-card>
 </template>
 
@@ -39,11 +58,54 @@
 export default {
   data () {
     return {
-
+      list: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.total {
+    border-bottom: 1px dashed #ccc;
+    height:50px;
+    line-height: 50px;
+}
+.article-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 10px;
+    border-bottom: 1px solid  #f2f3f5;
+    .left {
+        display: flex;
+        img {
+            width: 180px;
+            height:100px;
+            border-radius: 4px;
+        }
+        .info {
+            height: 100px;
+            display: flex;
+            flex-direction: column;
+            margin-left:10px;
+            justify-content: space-around;
+            .date {
+                color:#999;
+                font-size:12px;
+            }
+            .title {
+                font-size:14px;
+            }
+            .status {
+                width: 60px;
+                text-align: center
+            }
+        }
+    }
+    .right {
+        font-size: 12px;
+        span {
+            margin-right:8px;
+        }
+    }
+}
 </style>
