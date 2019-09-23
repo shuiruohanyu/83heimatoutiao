a9e3b209cb0bbeda2747940b61009a0fcf8f996b
shuiruohanyu <shuiruohanyu@foxmail.com>
2019-09-23 11:17:46 +0800

    黑马头条PC-内容列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -28,6 +28,9 @@ export default new Router({
         }, {
           path: 'material',
           component: () => import('./views/material')
+        }, {
+          path: 'articles',
+          component: () => import('./views/articles')
         }
       ]
     }, {
---
+++ src/views/articles/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-card>
+        <!--面包屑 -->
+        <bread-crumb slot='header'>
+           <template slot='title'>
+               内容列表
+           </template>
+        </bread-crumb>
+    </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
