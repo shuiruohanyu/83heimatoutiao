0ace0459f57ecd10737af9a8c9cf6fc2849b5795
shuiruohanyu <shuiruohanyu@foxmail.com>
2019-09-23 11:41:34 +0800

    黑马头条PC-内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,15 +6,44 @@
                内容列表
            </template>
         </bread-crumb>
+        <!-- 表单 -->
+        <el-form style="margin-left:50px">
+            <!-- 文章状态 -->
+            <el-form-item label="文章状态:">
+                 <el-radio-group>
+                   <el-radio>全部</el-radio>
+                   <el-radio>草稿</el-radio>
+                   <el-radio>待审核</el-radio>
+                   <el-radio>审核通过</el-radio>
+                   <el-radio>审核失败</el-radio>
+
+            </el-radio-group>
+            </el-form-item>
+            <el-form-item label="频道列表:">
+                <!-- 频道列表 -->
+                <el-select>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="时间选择:">
+                 <el-date-picker
+                   type="daterange"
+                   start-placeholder="开始日期"
+                   end-placeholder="结束日期">
+          </el-date-picker>
+            </el-form-item>
+        </el-form>
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
 
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
