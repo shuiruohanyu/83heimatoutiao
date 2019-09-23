293e0c0a48526fb4bde110d8188dc12dc79bbc14
gaoly <shuiruohanyu@foxmail.com>
2019-09-23 17:01:40 +0800

    黑马头条PC-内容列表-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,7 +6,6 @@
                内容列表
            </template>
         </bread-crumb>
-        {{formData}}
         <!-- 表单 -->
         <el-form style="margin-left:50px">
             <!-- 文章状态 -->
@@ -43,7 +42,7 @@
             </el-form-item>
         </el-form>
         <!-- 主体内容 -->
-     <div class='total'>共找到55091条符合条件的内容</div>
+     <div class='total'>共找到{{page.total}}条符合条件的内容</div>
      <div class='article-item' v-for="(item,index) in list" :key="index">
          <!-- 布局 -->
          <!-- 左侧 -->
@@ -62,6 +61,17 @@
             <span><i class="el-icon-delete"></i>删除</span>
         </div>
      </div>
+     <!-- 分页组件 -->
+     <el-row type='flex' justify="center" style='margin:10px 0'>
+       <el-pagination
+  background
+  @current-change="changePage"
+  :current-page="page.currentPage"
+  :page-size="page.pageSize"
+  layout="prev, pager, next"
+  :total="page.total">
+</el-pagination>
+     </el-row>
     </el-card>
 </template>
 
@@ -76,7 +86,12 @@ export default {
       },
       list: [],
       channels: [], // 定义一个频道数组
-      defaultImg: require('../../assets/img/default.gif') // 将图片地址转成base64
+      defaultImg: require('../../assets/img/default.gif'), // 将图片地址转成base64
+      page: {
+        total: 0,
+        currentPage: 1,
+        pageSize: 10
+      }
     }
   },
   methods: {
@@ -84,13 +99,24 @@ export default {
     changeCondition () {
       // 因为值改变时 formdata已经是最新的值 所以直接可以用formData的值请求
       // 组装请求参数
+      this.page.currentPage = 1 // 条件改变 默认会第一页
+      this.queryArticles()
+    },
+    // 切换页码
+    changePage (newPage) {
+      // 分页时要携带条件
+      this.page.currentPage = newPage // 赋值最新页码
+      this.queryArticles()
+    },
+    queryArticles () {
       let params = {
         status: this.formData.status === 5 ? null : this.formData.status, // 状态  如果为5时，就是全部，但是接口要求全部不传内容 null就相当于什么都没传
         channel_id: this.formData.channel_id, // 频道id
         begin_pubdate: this.formData.date.length ? this.formData.date[0] : null,
-        end_pubdate: this.formData.date.length > 1 ? this.formData.date[1] : null // 结束时间
+        end_pubdate: this.formData.date.length > 1 ? this.formData.date[1] : null, // 结束时间
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
       }
-
       this.getArticles(params)
     },
     getArticles (params) {
@@ -99,6 +125,7 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results // 获取文章列表
+        this.page.total = result.data.total_count // 赋值记录总数
       })
     },
     // 获取频道列表
