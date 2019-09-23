5c7650d32cc25553eda3d7df1b3795323a04e06c
gaoly <shuiruohanyu@foxmail.com>
2019-09-23 17:16:24 +0800

    黑马头条PC-发表文章-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -31,6 +31,9 @@ export default new Router({
         }, {
           path: 'articles',
           component: () => import('./views/articles')
+        }, {
+          path: 'publish',
+          component: () => import('./views/publish')
         }
       ]
     }, {
---
+++ src/views/publish/index.vue
@@ -0,0 +1,17 @@
+<template>
+   <el-card>
+       <bread-crumb slot='header'>
+          <template slot='title'>发布文章</template>
+       </bread-crumb>
+   </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
