86bd64a7fce6defade7c7f649fdec85a593638c6
gaoly <shuiruohanyu@foxmail.com>
2019-09-24 17:02:49 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,28 +6,49 @@
         <!-- 表单 -->
         <el-form style='margin-left:60px' label-width="100px">
             <el-form-item label="用户名">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.name" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="简介">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.intro" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="邮箱">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.email" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="手机号">
-                <el-input style="width:300px"></el-input>
+                <!-- 手机号不能修改 -->
+                <el-input disabled v-model="formData.mobile" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type='primary'>保存信息</el-button>
             </el-form-item>
         </el-form>
-        <img class='head-img' src="../../assets/img/default.gif" alt="">
+        <img class='head-img' :src="formData.photo ? formData.photo : defaultImg " alt="">
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      defaultImg: require('../../assets/img/default.gif'),
+      formData: {
 
+      }
+    }
+  },
+  methods: {
+    //   获取用户的个人信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
